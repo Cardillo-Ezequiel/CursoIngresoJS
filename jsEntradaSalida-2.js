6cbaca7c5857f94b384d89ec32acd2015fec5e62
Cardillo-Ezequiel <36281547+Cardillo-Ezequiel@users.noreply.github.com>
2018-02-08 20:40:03 -0300

    ejercicio 1 y 2
    alert , prompt

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,21 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+// defino una variable
+var nombre;
+alert(nombre);
+nombre=prompt("ingrese su nombre","natalia natalia");
+alert(nombre);
+
+
+
+/*
+nombre="ezequiel";
+alert(nombre);
+alert("nombre");
+*/
+//	var person = prompt("Please enter your name", "Harry Potter");
+	
+}
+
