3973284c466f1afa7d0e51c187cf99c00c4c327b
Cardillo-Ezequiel <36281547+Cardillo-Ezequiel@users.noreply.github.com>
2018-02-16 21:48:26 -0300

    ejercicios 2,3,4,5,6,
    el "2"lo hice de nuevo para hacerlo de una manera mas abreviada y ordenada; y el resto de los ejercicios los hice usando el mismo orden que lleva el "2" es decir de la manera corta y prolija

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -4,29 +4,23 @@ function Mostrar()
 	var mesDelAño = document.getElementById('mes').value;
 
 	switch(mesDelAño){
-		case"Enero": alert("Falta para el invierno.");
-		break;
-		case"Febrero": alert("Falta para el invierno.");
-		break;
-		case"Marzo": alert("Falta para el invierno.");
-		break;
-		case"Abril": alert("Falta para el invierno.");
-		break;
-		case"Mayo": alert("Falta para el invierno.");
-		break;
-		case"Junio": alert("Falta para el invierno.");
-		break;
-		case"Julio": alert("Abrigate que hace frio!!!");
-		break;
-		case"Agosto": alert("Abrigate que hace frio!!!");
-		break;
-		case"Septiembre": alert("Ya pasamos el frio, ahora calor!!!");
-		break;
-		case"Octubre": alert("Ya pasamos el frio, ahora calor!!!");
-		break;
-		case"Noviembre": alert("Ya pasamos el frio, ahora calor!!!");
-		break;
-		case"Diciembre": alert("Ya pasamos el frio, ahora calor!!!");
+		case"Enero":
+		case"Febrero":
+		case"Marzo":
+		case"Abril":
+		case"Mayo":
+		case"Junio": 
+			alert("Falta para el invierno.");
+			break;
+		case"Julio":
+		case"Agosto": 
+			alert("Abrigate que hace frio!!!");
+			break;
+		case"Septiembre":
+		case"Octubre":
+		case"Noviembre":
+		case"Diciembre":
+		alert("Ya pasamos el frio, ahora calor!!!");
 		break;
 	}
 	
