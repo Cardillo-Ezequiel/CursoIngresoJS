f72f66c97b2783196b5633b7bbb6d5fc2542fe24
Cardillo-Ezequiel <36281547+Cardillo-Ezequiel@users.noreply.github.com>
2018-02-16 21:25:58 -0300

    tp numero 1
    hicimos que el el juego tenga limite de intentos y luego que a la primera de acierto cambies el numero a otro random, como una "trampa".

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -9,18 +9,47 @@ de no ser igual se debe informar si “falta…”  para llegar al número secre
 */
 var numeroSecreto; 
 var contadorIntentos;
+var numeroIngresado;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
+	 numeroSecreto=Math.floor((Math.random() * 100) + 1);
+	 alert(numeroSecreto);
 		//alert(numeroSecreto );
-	
+	contadorIntentos=0;
 
 }
 
 function verificar()
 {
+	numeroIngresado=parseInt(document.getElementById('numero').value);
+	if (numeroSecreto==numeroIngresado&&contadorIntentos<1) {
+		alert("Juega de vuelta");
+		contadorIntentos=contadorIntentos+1;
+	}
+	 else {
+	if (contadorIntentos<10) {
 	
-	
+	if (numeroSecreto==numeroIngresado) 
+		{
+			alert("Usted es un ganador!!! y en solo "+contadorIntentos+ " intentos");
+
+		} else 
+			{
+				if (numeroSecreto>numeroIngresado) 
+				{
+					alert("falta");
+				}
+				 else 
+				{
+			 		alert("se paso");
+				}
+			
+			}
+			contadorIntentos=contadorIntentos+1;
+	document.getElementById('intentos').value=contadorIntentos;	
+}
+}
+
 }
