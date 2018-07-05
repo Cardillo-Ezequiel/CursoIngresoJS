780c92dac956841c60b948d59b827dc868da2f65
Cardillo-Ezequiel <36281547+Cardillo-Ezequiel@users.noreply.github.com>
2018-07-05 11:24:53 -0300

    primer commit
    estos son los 4 ejercicios de la primera clase.
    el 5 lo hice aparte para refrescar

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -3,19 +3,14 @@ y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
 // defino una variable
-var nombre;
-alert(nombre);
-nombre=prompt("ingrese su nombre","natalia natalia");
-alert(nombre);
-
-
-
-/*
-nombre="ezequiel";
-alert(nombre);
-alert("nombre");
-*/
-//	var person = prompt("Please enter your name", "Harry Potter");
-	
+	var nombre;
+	//nombre="Ezequiel";
+	nombre=prompt("ingrese su nombre")
+	alert("Su nombre es: "+nombre);
+	//alert(nombre);
+	//nombre=prompt("Ingrese su nombre");
+	//alert(nombre);	
+	//var person = prompt("Please enter your name", "Harry Potter");
+	//esto lo sacamos de w3school 
 }
 
