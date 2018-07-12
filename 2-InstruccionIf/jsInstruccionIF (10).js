b56fa7d7becf20bec493bffb873ded765700ad1d
Cardillo-Ezequiel <36281547+Cardillo-Ezequiel@users.noreply.github.com>
2018-07-12 12:11:08 -0300

    if 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,18 +1,21 @@
-function Mostrar()
+function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var nota;
-	
 	nota=Math.floor(Math.random() * 10) + 1;
-
-	if (nota>=9) {
-		alert("Exelente " + nota);
-	}
-	if (nota<=4&&nota>=8) {
-		alert("Aprobo " + nota);
-	}
-	if (nota<4) {
-		alert("Vamos, la proxima se puede " + nota);
-	}
+	if (nota>=9) 
+		{
+			alert("Exelente te sacaste un: "+nota);
+		} 
+		else 
+		{
+			if (nota>=4&&nota<=8) 
+				{
+					alert("Aprovaste con un: "+nota);
+				} else 
+				{
+					alert("Vamos, la proxima se puede: "+nota);				
+				}
+		}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,7 +2,9 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var boton;
-	boton=document.getElementById('FormIngreso').value;
-	alert(Math.floor(Math.random() * 10) + 1);
+	/*boton=document.getElementById('FormIngreso').value;
+	alert(Math.floor(Math.random() * 10) + 1);*/
+	boton=Math.floor(Math.random() * 10) + 1;
+	alert(boton);
 
 }//FIN DE LA FUNCIÓN
