0da7338749f835dd09b1e4bc503b487f85422a23
Cardillo-Ezequiel <36281547+Cardillo-Ezequiel@users.noreply.github.com>
2018-07-27 12:25:26 -0300

    ejercicio 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,63 +1,76 @@
-function Mostrar()
+function mostrar()
 {
 
 	var contador=0;
-	var numero;
-	var negativo=0;
-	var positivo=0;
-	var contadorPositivo=0;
-	var contadorNegativo=0;
-	var contadorCero=0;
-	var numeroPar=0;
-	var promedioNegativo=0;
-	var promedioPositivo=0;
-	var difPositivoNegativo;
-
 	//declarar contadores y variables 
-	
+	var numero;
 	var respuesta="si";
+	var negativo;
+	var positivo;
+	var contadorPositivo;
+	var contadorNegativo;
+	var cero;
+	var contadorPar;
+	var promedioP;
+	var promedioN;
+	var diferenciaNegativoYPositivo;
+
+	diferenciaNegativoYPositivo=0;
+	contadorPar=0;
+	promedioNegativo=0;
+	promedioPositivo=0;
+	contadorNegativo=0;
+	contadorPositivo=0;
+	positivo=0;
+	negativo=0;
+	cero=0;
+
 
 	while(respuesta!="no")
 	{
-		contador=contador+1;
 		numero=prompt("ingrese numero");
 		numero=parseInt(numero);
-		if (numero<0) 
-		{
-			negativo=negativo+numero;
-			contadorNegativo=contadorNegativo+1;
+		
 
-		}
-		if(numero>0)
-		{
-			positivo=positivo+numero;
-			contadorPositivo=contadorPositivo+1;
-		}
-		else
-		{
-			contadorCero=contadorCero+1;
-		}
-		if (numero%2==0) 
-		{
-			numeroPar=numeroPar+1;
-		}
-	respuesta=prompt("para salir, no");
+
+		if (numero==0) 
+			{
+				cero++;
+			} 
+			else 
+			{
+				
+						if (numero<0) 
+					{
+						negativo=negativo+numero;
+						contadorNegativo++;
+						
+					} 
+					else 
+					{
+						positivo=positivo+numero;
+						contadorPositivo++;
+					
+					}
+					
+				
+			}
+			if (numero%2==0) 
+			{
+				contadorPar++;
+			}
+
+
+
+
+
+		respuesta=prompt("ingrese no para salir");
 	}
 
 	promedioPositivo=positivo/contadorPositivo;
 	promedioNegativo=negativo/contadorNegativo;
+	diferenciaNegativoYPositivo=positivo-negativo;
 
-	difPositivoNegativo=positivo-negativo;
-
-
-	document.write("1) Numeros positivos: "+positivo+"<br>"+"2) Numeros negativos: "
-					+negativo+"<br>"+"3) Cantidad de numeros positivos:"+contadorPositivo+
-					"<br>"+"4) Cantidad de numeros negativos: "+contadorNegativo+"<br>"+
-					"5) Cantidad de ceros: "+contadorCero+"<br>"+"6) Cantidad de numeros pares: "+numeroPar
-					+"<br>"+"7) Promedio de numeros positivos: "+promedioPositivo+"<br>"
-					+"8) Promedio de numeros negativos: "+promedioNegativo+"<br>"+
-					"9) Diferencia entre positivos y negativos: "+difPositivoNegativo);
-
-
-
+	document.write("La suma de los negativos es: "+negativo+"<br>"+"La suma de los positivos es: "+positivo+"<br>"+"La cantidad de numeros negativos es: "+contadorNegativo+"<br>"+"La cantidad de numeros positivos es: "+contadorPositivo+"<br>"+"La cantidad de ceros es: "+cero+"<br>"+"La cantidad de numeros pares es: "+contadorPar+"<br>"+"El promedio de los negativo es: "+promedioNegativo+"<br>"+"El promedio de los positivo es: "+promedioPositivo+"<br>"+"La diferencia entre negativos y positivos es: "+diferenciaNegativoYPositivo);
+	
 }//FIN DE LA FUNCIÓN
